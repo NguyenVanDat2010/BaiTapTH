f99d5e4e057061c04555ed230dbd783f89c4fc2d
Kira <datdhvinh@gmail.com>
2020-04-01 16:22:19 +0700

    01.04.2020

---
+++ W2/Buoi2/Currency/Currency.js
@@ -0,0 +1,18 @@
+// function showConvert() {
+//     let amount,fromCurrency,toCurrency,result;
+//     amount=document.getElementById("amount").value;
+//     fromCurrency=document.getElementById("fromCurrency").value;
+//     toCurrency=document.getElementById("toCurrency").value;
+//     if(fromCurrency == toCurrency){
+//         result="Không chuyển đổi";
+//     }
+//     else {
+//         if(fromCurrency == "usd" && toCurrency == "vnd"){
+//             result=amount*23000;
+//         }
+//         else{
+//             result=amount/23000;
+//         }
+//     }
+//     document.getElementById("result").innerText=result;
+// }
