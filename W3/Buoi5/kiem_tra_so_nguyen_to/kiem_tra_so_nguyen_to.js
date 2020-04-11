11cb5cf26b4395e8d125e209e168c07c06fe7572
Kira <datdhvinh@gmail.com>
2020-04-11 13:47:23 +0700

    so nguyen to finish

--- W3/Buoi5/kiem_tra_so_nguyen_to/kiem_tra_so_nguyen_to.js
+++ W3/Buoi5/kiem_tra_so_nguyen_to/kiem_tra_so_nguyen_to.js
@@ -1,3 +1,4 @@
+//Hàm kiểm tra số nguyên tố
 function checkNumber(n) {
     for (let i = 2; i <= Math.sqrt(n); i++) {
         if (n % i == 0) {
@@ -10,8 +11,31 @@ function checkNumber(n) {
     if (check == true) {
         document.write(n + ' là số nguyên tố')
     }else {
-        document.write(n + ' ko phai là số nguyên tố')
+        document.write(n + ' không phải là số nguyên tố')
     }
 }
+//Gọi hàm
+let anotherNumber=Number(prompt("Nhập một số nguyên bất kỳ: "));
+checkNumber(anotherNumber);
 
-checkNumber(12);
+//-----------------------------------------------------------------------------------------------------------------
+//tìm tất cả các số nguyên tố nhỏ hơn 10000.
+let num = 10000 , kiemtra = true , ketqua = '' , n = 2;
+document.write("Các số nguyên tố đầu tiên nhỏ hơn "+num+" là: "+'<br>');
+while (n < num) {
+    for (let i = 2 ; i <= (n / 2) ; i++) {
+        if (n % i == 0) {
+            kiemtra = false;
+            break;
+        } else {
+            kiemtra = true;
+        }
+    }
+    if (kiemtra == true) {
+        ketqua = n;
+        document.write(ketqua + ', ');
+    } else {
+        ketqua='';
+    }
+    n++;
+}
