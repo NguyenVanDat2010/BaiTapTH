6f534c092be18b730826978e727e2298376a727c
Kira <datdhvinh@gmail.com>
2020-04-12 13:57:08 +0700

    chua hoan thanh

--- W3/Buoi5/ung_dung_quan_ly_sp/ung_dung_quan_ly_sp.js
+++ W3/Buoi5/ung_dung_quan_ly_sp/ung_dung_quan_ly_sp.js
@@ -2,58 +2,74 @@
 //'Sony Xperia','Samsung galaxy s10','Xiaomi note 8','Iphone 11 pro max','Nokia 8','Huawei nova 3i','Oppo f1s'
 let arrA=[];
 let k=0,check=true,count=0;
-function addProducts() {
-    let inputNameOfProduct =document.getElementById("addproduct").value;
-    if (inputNameOfProduct==''){
-        alert("Bạn chưa nhập tên sản phẩm");
-        document.getElementById("addproduct").focus();
-        return;
-    }
-    for (let i=0;i<arrA.length;i++) {
-        if (inputNameOfProduct == arrA[i]) {
-            check=false;
-            break;
+
+//Tạo sp----------------------------------------------------------------------------------------------------------------
+function createProducts(newProducts) {
+    if (newProducts == '') {
+        return  false;
+    } else {
+        for (let i = 0 ; i < arrA.length ; i++) {
+            if (newProducts == arrA[i]) {
+                return false;
+            }
         }
-    }
-    if (check==true) {
-        arrA[k] = inputNameOfProduct;
-        alert("Thêm sản phẩm " + arrA[k] + " thành công");
-        k++;count++;
-        document.getElementById("addproduct").value = '';
-
-        //Hien thi sp
-        let table = '<table border="1" cellspacing="0">';
-        table += '<tr bgcolor="#adff2f" align="center">';
-        table += '<td width="35px">' + "STT" + '</td>';
-        table += '<td width="250px">' + "Tên sản phẩm" + '</td>';
-        table += '<td width="100px">' + "Sửa" + '</td>';
-        table += '<td width="100px">' + "Xóa" + '</td>';
-        table += '</tr>';
-
-        for (let j = 0 ; j < arrA.length ; j++) {
-            table += '<tr>';
-            table += '<td>' + (j + 1) + '</td>';
-            table += '<td>' + arrA[j] + '</td>';
-            table += '<td>' + "<input type='button' value='Sửa' onclick='editProducts();'>" + '</td>';
-            table += '<td>' + "<input type='button' value='Xóa' onclick='deleteProducts();'>" + '</td>';
-            table += '</tr>';
-
-        }
-        table += '</table>';
-        document.getElementById("result").innerHTML = table;
-        document.getElementById("countproducts").innerHTML =count;
-    }else {
-        alert("Tên sản phẩm đã tồn tại");
-        document.getElementById("addproduct").focus();
+        arrA.push(newProducts);
+        return true;
     }
 }
+
+//Thêm sp----------------------------------------------------------------------------------------------------------------
+function addProducts() {
+    let inputNameOfProduct = document.getElementById("addproduct").value;
+    if (createProducts(inputNameOfProduct)){
+        alert("Thêm sản phẩm " + arrA[count] + " thành công");
+        count++;
+        document.getElementById("addproduct").value = '';
+    }else {
+        alert("Bạn chưa nhập tên sản phẩm hoặc tên sản phẩm đã tồn tại");
+        document.getElementById("addproduct").focus();
+    }
+    showProducts();
+}
+
+//Sửa sp----------------------------------------------------------------------------------------------------------------
 function editProducts() {
 
 }
-function deleteProducts(abc) {
-    for (let x = 0 ; x < arrA.length ; x++) {
-        if (arrA[abc] == arrA[x]) {
-            arrA.splice(x , 1)
+
+//Xóa sp----------------------------------------------------------------------------------------------------------------
+function deleteProducts(index) {
+    for (let i=0;i<arrA.length;i++) {
+        if (index == i) {
+            arrA.splice(i , 1);
+            break;
         }
     }
+    //count=arrA.length;
+    showProducts();
 }
+
+//Hien thi sp-----------------------------------------------------------------------------------------------------------
+function showProducts() {
+    let table = '<table border="1" cellspacing="0">';
+    table += '<tr bgcolor="#adff2f" align="center">';
+    table += '<td width="35px">' + "STT" + '</td>';
+    table += '<td width="250px">' + "Tên sản phẩm" + '</td>';
+    table += '<td width="100px">' + "Sửa" + '</td>';
+    table += '<td width="100px">' + "Xóa" + '</td>';
+    table += '</tr>';
+    for (let j = 0 ; j < arrA.length ; j++) {
+        table += '<tr>';
+        table += '<td>' + (j + 1) + '</td>';
+        table += '<td>' + arrA[j] + '</td>';
+        table += '<td>' + "<input type='button' value='Sửa' onclick='editProducts();'>" + '</td>';
+        table += '<td>' + "<input type='button' value='Xóa' onclick='deleteProducts(j);'>" + '</td>';
+        table += '</tr>';
+    }
+    table += '</table>';
+    document.getElementById("result").innerHTML = table;
+    document.getElementById("countproducts").innerHTML =count;
+}
+
+
+
